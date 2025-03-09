cb75ec7149e47fe28a84d1b86ec554933947b8f4
Ian <gamrise@gmail.com>
2025-03-09 16:15:12 -0700

    installed React Router Dom package

--- vetapp/src/App.jsx
+++ vetapp/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/header'
+import {Route, Routes} from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0)
