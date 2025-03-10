bba2ed2a3eae279528a83fa85805a34c02651824
Ian <gamrise@gmail.com>
2025-03-09 18:35:55 -0700

    Attempted to add Aboutpage to app.jsx with Routing

--- vetapp/src/App.jsx
+++ vetapp/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Header from './components/header'
 import {Route, Routes} from "react-router-dom"
+import AboutPage from './components/aboutpage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,9 @@ function App() {
   return (
     <>
     <Header/>
+    <Routes>
+      <Route path= "/"element = {<AboutPage/>} />
+      </Routes>
     </>
   )
 }
