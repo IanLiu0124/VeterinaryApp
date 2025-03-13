2e28f1756e2d0310ebe5457aa11f6dc5805f2214
Ian <andyu0124@gmail.com>
2025-03-13 16:45:30 -0700

    still figuring out the positions of the form

--- vetapp/src/components/addmedication.jsx
+++ vetapp/src/components/addmedication.jsx
@@ -1,28 +1,29 @@
 const AddMedication = () => {
     return ( <div className="col-sm" style={{border:"white solid 1px", padding:"1.3em 1.3em"}}>
         <h5>Add Medication</h5>
-        <form>
+        <form className="mb-3">
             <div className="form-row align-items-left">
-                <div className="col">   
+                <div className="col-6">   
         <label for="drugName">Name: </label>
         <input className="form-control-sm" type="text" id="name" name="inputName"></input>
         </div>
-        <div className="col form-check">
+        <div className="form-check col-3">
+            
+            <input type="checkbox" className="form-check-input" name="canineDrug" id="canineDrug"></input>
             <label for="canineDrug" className="form-check-label">Canine</label>
-            <input type="checkbox" name="canineDrug" id="canineDrug"></input>
             </div>
-            <div className="col form-check">
+            <div className="form-check col-3">
+            <input className="form-check-input"  type="checkbox" name="felineDrug" id="felineDrug"></input>
             <label for="felineDrug" className="form-check-label">Feline</label>
-            <input type="checkbox" name="felineDrug" id="felineDrug"></input>
             </div>
-            <div className="col">
-        <label for="drugDosage">Weight: </label>
+            <div className="col-7">
+        <label for="drugDosage">Dosage per Kg: </label>
         <input className="form-control-sm" type="number" id="weight" name="weight" placeholder="kg"></input>
         </div>
-        <div className="col">
+        <div className="col-4">
         <select className="form-control-sm" name="specie">
-            <option>Feline</option>
-            <option>Canine</option>
+            <option>ml</option>
+            <option>mg</option>
         </select>
         </div>
         </div>
