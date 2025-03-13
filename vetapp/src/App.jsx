d03c5eaa5fdab15232e262b45ddd827fb0a5b4e4
Ian <gamrise@gmail.com>
2025-03-12 21:57:55 -0700

    added a table template for patientoverview, fixed margins

--- vetapp/src/App.jsx
+++ vetapp/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/header'
 import {Route, Routes} from "react-router-dom"
 import AboutPage from './components/aboutpage'
 import NavBar from './components/navbar'
+import OverView from './components/overviewpage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
     <Header/>
     <NavBar/>
     <Routes>
+      <Route path= "/"element = {<OverView/>} />
       <Route path= "/about"element = {<AboutPage/>} />
       </Routes>
     </>
--- vetapp/src/components/overviewpage.jsx
+++ vetapp/src/components/overviewpage.jsx
@@ -1,5 +1,33 @@
+import Table from 'react-bootstrap/Table';
+
 const OverViewPage = () => {
-    return ( <div></div> );
+    return (
+        <div style={{ margin: "3em" }}>
+            <h4>Patient List</h4>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Species</th>
+                        <th>Weight</th>
+                        <th>Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* {patients.map((patient) => (
+                        <tr key={patient.id}>
+                            <td>{patient.name}</td>
+                            <td>{patient.species}</td>
+                            <td>{patient.weight} kg</td>
+                            <td>
+                                <Link to={`/patient/${patient.id}`}>View</Link>
+                            </td>
+                        </tr>
+                    ))} */}
+                </tbody>
+            </Table>
+        </div>
+    );
 }
  
 export default OverViewPage;
