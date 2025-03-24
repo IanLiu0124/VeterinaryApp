ff03672bcb87ae987bbaf75bdb4a81f38dd9ff18
Ian <gamrise@gmail.com>
2025-03-24 01:28:34 -0700

    added add button to addpatient

--- vetapp/src/components/addpatient.jsx
+++ vetapp/src/components/addpatient.jsx
@@ -1,3 +1,5 @@
+import Button from 'react-bootstrap/Button'
+
 const AddPatient = () => {
     return ( <div className="col-sm" style={{border:"white solid 1px", padding:"1.3em 1.3em"}}>
         <h5>Add Patient</h5>
@@ -12,12 +14,16 @@ const AddPatient = () => {
         <input className="form-control-sm" type="number" id="weight" name="weight" placeholder="kg"></input>
         </div>
         <div className="col">
+
         <select className="form-control-sm" name="specie">
             <option>Feline</option>
             <option>Canine</option>
         </select>
         </div>
         </div>
+        <div className="d-grid gap-2 container mt-5">
+    <Button variant="outline-light" size="lg">Add</Button>
+    </div>
     </form>
     </div> );
 }
