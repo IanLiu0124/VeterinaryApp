95d4e0224a7faee410e1545dd8e6ea20a51ef628
Ian <andyu0124@gmail.com>
2025-03-28 17:39:07 -0700

    Completely reworked medication schema and incorperated it into tables

--- vetapp/src/components/medication.jsx
+++ vetapp/src/components/medication.jsx
@@ -5,9 +5,9 @@ const Medication = ({medication, weight, removeMedBtn = (p) => p, specie}) => {
     
     return ( <tr>
         <td>{medication.name}</td>
-        <td>{medication.concentration}<br></br>{medication.form}</td>
-        <td><strong>{(medication.dosages[specie].dosage_per_kg * weight).toFixed(2)} {medication.dosages.unit}</strong>
-            <p>({medication.dosages[specie].dosage_per_kg} {medication.dosages.unit} per kg)</p></td>
+        <td>{medication.concentration.per_unit}{medication.dosages.unit}/{medication.concentration.unit}<br></br>{medication.form}</td>
+        <td><strong>{((medication.dosages[specie].unit_per_kg * weight) / medication.concentration.per_unit).toFixed(2)} {medication.concentration.unit}</strong>
+            <p>({medication.dosages[specie].unit_per_kg} {medication.dosages.unit} per kg)</p></td>
         <td className='removeUnderLineLink'><Button variant='dark' onClick ={()=>removeMedBtn(medication.name)}><i className="bi bi-x-octagon"></i></Button></td>
     </tr> );
 }
